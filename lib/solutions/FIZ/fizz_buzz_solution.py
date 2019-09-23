c91a2f6779633929cc3a4b83acc8f64476c78919
sbx_user1051 <sbx_user1051@169.254.140.133>
2019-09-23 19:58:50 +0000

    Mon Sep 23 19:58:50 UTC 2019

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,9 +1,10 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
-    number=""+number
-
+   
+    number_str = str(number)
+    number = number
     if number%3==0 and number.find("3")>0:
         return "fizz"
-
+    elif number_str.find("3") > 0 or number % 3 == 0:
     else:
         return number
